dbf36e4d21654244349d2f748a78f994058c8109
Dan Bickford <danbickford007@yahoo.com>
2013-06-04 11:48:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doubleoweb::Application.config.secret_token = 'b6597c2b024cf8b950576d53b7f6ac9509646b268e5305ced3c46b67d6c8acfd5ed642255fdee38ae9ea4e4c7e9615023c65ed97fcf2cd7415dadb20f63f24c5'
